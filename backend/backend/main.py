adbf117f4009cdd612a9ccadf06e764246caf43c
Bosire Nyakundi <nyakundi@hotmail.co.uk>
2023-11-15 01:38:01 +0000

    Removed failing import

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routers.candidate import router as candidate_speaking_router
-from backend.routers.interviewer import router as interviewer_speaking_router
 from backend.routers.conversation.conversation import router as conversation_router
 
 app = FastAPI()
 
 app.include_router(candidate_speaking_router)
-app.include_router(interviewer_speaking_router)
 app.include_router(conversation_router)
 
 app.add_middleware(
@@ -17,3 +15,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
