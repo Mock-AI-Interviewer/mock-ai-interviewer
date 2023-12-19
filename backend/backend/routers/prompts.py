6e70348ed52a4b7358129c07b1fffe558a7a2252
Seth Odia <60739582+sethGGO@users.noreply.github.com>
2023-12-19 11:16:49 +0000

    #47 Removed unused endpoints

--- backend/backend/routers/prompts.py
+++ backend/backend/routers/prompts.py
@@ -42,27 +42,6 @@ async def update_coding_interview_type_init_prompt(new_prompt: str = Path(...)) 
     except models.NotFoundError as e:
         raise HTTPException(status_code=404, detail="Interview type not found")
 
-@router.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: UUID):
-    if item_id in items:
-        return items[item_id]
-    raise HTTPException(status_code=404, detail="Item not found")
-
 @router.get("/items", response_model=List[models.InterviewTypeSummary])
 async def read_all_items():
     return service.list_all_interview_type_summaries()
-
-@router.put("/items/{item_id}", response_model=Item)
-async def update_item(item_id: UUID, item: Item):
-    if item_id in items:
-        item.id = item_id
-        items[item_id] = item
-        return item
-    raise HTTPException(status_code=404, detail="Item not found")
-
-@router.delete("/items/{item_id}", response_model=Item)
-async def delete_item(item_id: UUID):
-    if item_id in items:
-        del items[item_id]
-        return {"detail": "Item deleted"}
-    raise HTTPException(status_code=404, detail="Item not found")
