41a4be704cac9145e0c4774a777a9eba4097fee6
Bosire Nyakundi <nyakundi@hotmail.co.uk>
2024-02-08 06:26:23 +0000

    #53 updating runlocal

--- scripts/get-service-selection.sh
+++ scripts/get-service-selection.sh
@@ -2,7 +2,7 @@
 
 # Function to get selected services from user
 get_service_selection() {
-    echo "Enter the service names separated by space (e.g., frontend backend):" >&2
+    echo "Enter the service names separated by space, or leave empty to run for all services (e.g., frontend backend):" >&2
     read -ra SERVICES # Read services into an array
     echo "${SERVICES[@]}" # Output the selected services
 }
